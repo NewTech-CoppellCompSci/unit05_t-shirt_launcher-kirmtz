182a6d371127cfd4f0e0e481167c9473601b8065
kirmtz <111779940+kirmtz@users.noreply.github.com>
2023-01-09 11:36:25 -0600

    Update Lab01.java

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -19,6 +19,15 @@ public class Lab01 {
 	
 	public static void problem1() {
 	
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter a positive integer: ");
+		int first = inKey.nextInt();
+		
+		System.out.println("Enter another positive integer");
+		int second = inKey.nextInt();
+		
+		System.out.println(first + "^" + second + " = " + Math.pow(first, second) + "\n"); 
+		
 		
 	}
 	
@@ -26,12 +35,35 @@ public class Lab01 {
 	
 	public static void problem2() {
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter a positive number: ");
+		int input = inKey.nextInt();
+		
+		System.out.println("The square root of " + input + " is " + Math.sqrt(input));
 			
 	}
 	
 	
 	
 	public static void problem3() {
+		
+		System.out.println ("Give me the sides for a right triangle ");
+		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Length of side A: ");
+		int sideA = inKey.nextInt();
+		
+		int second = 2;
+		
+		System.out.println("Length of side B: ");	
+		int sideB = inKey.nextInt();
+		
+		
+		
+		int sideAB = sideA + sideB;
+		
+		System.out.println("Hypotenuse = " + Math.pow(sideA, second) + 
+				Math.pow(sideB, second)   );
 		
 		
 	}
@@ -41,8 +73,7 @@ public class Lab01 {
 	public static void problem4() {
 		
 		
-	}
 	
-	
+}
 	
 }
