b44501c4eeffb2a43ea8653c28ac6e39ae88420d
kirmtz <111779940+kirmtz@users.noreply.github.com>
2023-01-11 15:54:22 -0600

    Update Performance_Task.java

--- src/performance_task/Performance_Task.java
+++ src/performance_task/Performance_Task.java
@@ -1,11 +1,44 @@
 package performance_task;
 
+import java.util.Scanner;
+
 public class Performance_Task {
 
 	public static void main(String[] args) {
 		
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter launch velocity (m/s): ");
+		double lv = inKey.nextDouble();
 		
+		System.out.println("Enter launch angle (degrees): ");
+		double la = inKey.nextDouble();
+		
+		System.out.println("Projectile's path: ");
+		
+		int time = 0;
+		double x = 0;
+		double y = 0;
+		
+		do {
+			System.out.println("Time: " + time + "s");
+			
+			x= Math.cos(Math.toRadians(la))*time* lv;
+			y = Math.sin(Math.toRadians(la))* time * lv - 0.5 *9.8 * Math.pow(time,2);
+			if(y< 0) {
+				break;
+				
+			}
+			
+			System.out.println("\tx-pos: " + x + "m");
+			System.out.println("\ty-pos: " + y + "m");
+			
+			time++;
+			
+		} while (y>=0);
+		
+		System.out.println("\tx-pos: " + x + "m");
+		System.out.println("\ty-pos: 0m" );
 		
 		
 		
